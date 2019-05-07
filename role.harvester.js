b1a42fb3332dc90018e68c45a50c62bf3f7759c1
Jovan Kanevche <kanevche.jovan@gmail.com>
2019-05-07 03:19:11 +0200

    Make harvesters fill out every structure that needs energy

--- role.harvester.js
+++ role.harvester.js
@@ -3,9 +3,7 @@ const creepCore = require('./creepCore')
 module.exports = creepCore({
     work: creep => {
         const structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-            filter: s =>
-                s.structureType == STRUCTURE_SPAWN &&
-                s.energy < s.energyCapacity
+            filter: s => s.energy < s.energyCapacity
         })
 
         if (structure != undefined) {
