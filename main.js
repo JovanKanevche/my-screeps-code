d3f0fe9e539b9ee2f9d8659438e25d94bf69fe09
Jovan Kanevche <kanevche.jovan@gmail.com>
2019-05-07 02:44:47 +0200

    Make main run builder

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 const harvester = require('./role.harvester')
-const upgrader = require('./role.upgrade')
+const upgrader = require('./role.upgrader')
+const builder = require('./role.builder')
+const spawn = require('./spawn')
 
 module.exports.loop = () => {
     for (let name in Game.creeps) {
         const creep = Game.creeps[name]
-
-        switch (creep.role) {
+        switch (creep.memory.role) {
             case 'harvester':
                 harvester.run(creep)
                 break
             case 'upgrader':
                 upgrader.run(creep)
                 break
+            case 'builder':
+                builder.run(creep)
+                break
         }
+
+        spawn.run(Game.spawns.Spawn1)
     }
 }
