20c897bb4a85cc32b571bf6d108a72afd3715cb0
Jovan Kanevche <kanevche.jovan@gmail.com>
2019-05-09 15:21:13 +0200

    Remove unused code from builder

--- role.builder.js
+++ role.builder.js
@@ -3,22 +3,14 @@ const creepCore = require('./creepCore')
 
 module.exports = creepCore({
     work: creep => {
-        const structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-            filter: s =>
-                s.structureType == STRUCTURE_SPAWN &&
-                s.energy < s.energyCapacity
-        })
+        const constructionSite = creep.pos.findClosestByPath(
+            FIND_CONSTRUCTION_SITES
+        )
 
-        if (structure != undefined) {
-            const constructionSite = creep.pos.findClosestByPath(
-                FIND_CONSTRUCTION_SITES
-            )
-
-            if (constructionSite != undefined) {
-                if (creep.build(constructionSite) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(constructionSite)
-                }
-            } else roleUpgrader.run(creep)
-        }
+        if (constructionSite != undefined) {
+            if (creep.build(constructionSite) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(constructionSite)
+            }
+        } else roleUpgrader.run(creep)
     }
 })
